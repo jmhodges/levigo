67a0cf253282e549907ec55d7fb602810b180db2
Jeff Hodges <jeff@somethingsimilar.com>
2012-03-26 03:48:42 -0700

    correct outputted import statement for newer godoc

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@ set -o nounset
 set -o errexit
 
 git checkout master
-godoc -html . > godoc.html
+# assumes that the repo is checked out in a GOPATH directory as
+# github.com/jmhodges/levigo
+godoc -html github.com/jmhodges/levigo > godoc.html
 git checkout gh-pages
 
 cat head.html godoc.html footer.html > index.html
